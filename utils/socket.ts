c98394c07f3185230bc5499c9354041dd8ea11d5
ChiThanh <ngchithanh777@gmail.com>
2025-12-06 18:09:43 +0700

    Resolve conflict socket file

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,14 +1,13 @@
 import { io } from "socket.io-client";
-import { Platform } from "react-native";
 
-// ⚙️ Dùng chung IP & PORT như apiClient
-const LOCAL_IP = "192.168.1.177 ";
-const PORT = 5050; 
+const LOCAL_IP = "192.168.1.46";
+const PORT = 5050;
 
-const SOCKET_URL =
-  Platform.OS === "android"
-    ? `http://${LOCAL_IP}:${PORT}` // Android emulator
-    : `http://${LOCAL_IP}:${PORT}`; // iOS / thiết bị thật
+const SOCKET_URL = `http://${LOCAL_IP}:${PORT}`;
+
+if (__DEV__) {
+  console.log("[socket] connecting to:", SOCKET_URL);
+}
 
 export const socket = io(SOCKET_URL, {
   transports: ["websocket"],
