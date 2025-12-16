d032159ad64ae4fa91191d58254935f8ef5faa7b
ChiThanh <ngchithanh777@gmail.com>
2025-12-16 22:34:35 +0700

    UpdateRoom

--- services/apiClient.ts
+++ services/apiClient.ts
@@ -4,7 +4,7 @@ import { Platform } from "react-native";
 
 // IP máy tính 
 
-const LOCAL_IP = "192.168.1.113";
+const LOCAL_IP = "192.168.1.147";
 const PORT = 5050;
 
 //  Cấu hình chính xác cho mọi môi trường
